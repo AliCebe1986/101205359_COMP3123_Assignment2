d1b1e44d7e82f59fcc0070c8022f460d1f4312a1
Ali Cebe <alicebe86@gmail.com>
2024-11-29 12:23:54 -0500

    Package-json edited

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const employeeRoutes = require('./routes/employeeRoutes');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
@@ -16,9 +15,9 @@ app.use('/api/users', userRoutes);
 app.use('/api/employees', employeeRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
